392978bc667813dc0f2fcffe38a11a8ff904d190
julianvasquez <julianvasquezcorredor@hotmail.com>
2020-09-17 12:06:24 -0500

    segundo commit

--- sistemaweb/Models/Producto.cs
+++ sistemaweb/Models/Producto.cs
@@ -8,8 +8,6 @@ namespace sistemaweb.Models
         public int Idproducto { get; set; }
         public int Idcategoria { get; set; }
         public string Codigo { get; set; }
-        public string Nombre { get; set; }
-        public decimal PrecioVenta { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
         public bool? Estado { get; set; }
